190d9ebf52e45d0ea45c587804172aafd7369132
Oelob <ab09@inbox.ru>
2024-03-09 10:21:37 +0500

    первая рабочая версия

--- src/main/java/com/example/demo/service/TelegremBot.java
+++ src/main/java/com/example/demo/service/TelegremBot.java
@@ -3,7 +3,9 @@ package com.example.demo.service;
 import com.example.demo.config.BotConfig;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
@@ -21,16 +23,36 @@ public class TelegremBot extends TelegramLongPollingBot {
 
         if (update.hasMessage() && update.getMessage().hasText()){
             String messageText = update.getMessage().getText();
-
+            long chatID = update.getMessage().getChatId();
             switch (messageText){
 
                 case ("/start"):
-
+                    startCommandReceived(chatID, update.getMessage().getChat().getFirstName());
+                    break;
+                default: sendMessage(chatID,  "извините, не работает");
             }
         }
 
+
     }
 
+    private void startCommandReceived(long chatID, String name){
+        String answer = "Hi " + name + "!";
+        sendMessage(chatID, answer);
+    }
+
+    private void sendMessage (long chatID, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(chatID);
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        }
+        catch (TelegramApiException e){
+
+        }
+    }
 
     @Override
     public String getBotUsername() {
